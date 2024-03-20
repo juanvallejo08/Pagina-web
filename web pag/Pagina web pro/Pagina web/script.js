c91dd12117764d41f9a9f180e81a0aa6af8e91d3
juanvallejo08 <131899233+juanvallejo08@users.noreply.github.com>
2024-03-20 18:10:19 -0500

    Segunda versión de la pagina web
    En esta versión de la pagina web se puede ver un avance significativo ya que tiene aspectos como lo son imágenes recurrentes y  las partes de una pagina principal de una pagina web

---
+++ web pag/Pagina web pro/Pagina web/script.js
@@ -0,0 +1,32 @@
+// let currentSlide = 0;
+// const slides = document.querySelectorAll(".slide");
+
+// function showSlide(n) {
+//   slides.forEach((slide) => {
+//     slide.style.display = "none";
+//   });
+//   slides[n].style.display = "block";
+// }
+
+// function nextSlide() {
+//   currentSlide++;
+//   if (currentSlide >= slides.length) {
+//     currentSlide = 0;
+//   }
+//   showSlide(currentSlide);
+// }
+
+// function prevSlide() {
+//   currentSlide--;
+//   if (currentSlide < 0) {
+//     currentSlide = slides.length - 1;
+//   }
+//   showSlide(currentSlide);
+// }
+
+// // Mostrar la primera imagen al cargar
+// showSlide(currentSlide);
+
+// // Avanzar y retroceder con botones
+// document.getElementById("nextBtn").addEventListener("click", nextSlide);
+// document.getElementById("prevBtn").addEventListener("click", prevSlide);
